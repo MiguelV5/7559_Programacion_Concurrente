f8ea47a0b4b532683474e1ad086f159f1658d7d8
MiguelV5 <mvasquezj@fi.uba.ar>
2023-11-19 13:30:20 -0300

    code cleaning

--- e_commerce/src/ec/sl_communicator.rs
+++ e_commerce/src/ec/sl_communicator.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 use std::sync::{mpsc, Arc};
 use std::thread::JoinHandle;
 
@@ -18,6 +19,7 @@ use crate::ec::{
     constants::{SL_INITIAL_PORT, SL_MAX_PORT},
     order_handler::AddSLMiddlemanAddr,
 };
+use shared::port_binder::listener_binder;
 
 use super::order_handler::OrderHandler;
 
@@ -71,61 +73,100 @@ pub fn setup_locals_connection(
     rx_from_input: mpsc::Receiver<String>,
 ) -> JoinHandle<()> {
     std::thread::spawn(move || {
-        if let Ok((listener, listener_addr)) =
-            shared::port_binder::listener_binder::try_bind_listener(SL_INITIAL_PORT, SL_MAX_PORT)
-        {
-            info!("Iniciado listener en {}", listener_addr);
-            let _ = listener.set_nonblocking(true);
-            let mut local_shop_id = 1;
-            loop {
-                if let Ok(msg) = rx_from_input.try_recv() {
-                    if msg == EXIT_MSG {
-                        info!("Received exit msg from input_handler, stopping listener");
-                        if let Some(system) = System::try_current() {
-                            system.stop()
-                        }
-                        return;
-                    }
-                }
+        if let Err(e) = handle_incoming_locals(order_handler, rx_from_input) {
+            error!("{}", e);
+        };
+    })
+}
 
-                if let Ok((stream, stream_addr)) = listener.accept() {
-                    if let Ok(async_stream) = AsyncTcpStream::from_std(stream) {
-                        info!(" [{:?}] Cliente conectado", stream_addr);
-                        let (read_half, write_half) = split(async_stream);
+fn handle_incoming_locals(
+    order_handler: Addr<OrderHandler>,
+    rx_from_input: mpsc::Receiver<String>,
+) -> Result<(), String> {
+    if let Ok((listener, listener_addr)) =
+        listener_binder::try_bind_listener(SL_INITIAL_PORT, SL_MAX_PORT)
+    {
+        info!("Iniciado listener en {}", listener_addr);
+        listener
+            .set_nonblocking(true)
+            .map_err(|err| err.to_string())?;
 
-                        let sl_middleman_addr = SLMiddleman::create(|ctx| {
-                            SLMiddleman::add_stream(
-                                LinesStream::new(BufReader::new(read_half).lines()),
-                                ctx,
-                            );
-                            SLMiddleman {
-                                local_shop_write_stream: Arc::new(Mutex::new(write_half)),
-                                local_shop_id,
-                            }
-                        });
+        handle_communication_loop(rx_from_input, listener, order_handler)
+    } else {
+        if let Some(system) = System::try_current() {
+            system.stop()
+        }
+        Err("Error al intentar bindear el puerto".to_string())
+    }
+}
 
-                        match order_handler.try_send(AddSLMiddlemanAddr {
-                            sl_middleman_addr: sl_middleman_addr.clone(),
-                            local_shop_id,
-                        }) {
-                            Ok(_) => {}
-                            Err(e) => {
-                                error!(" Error al enviar AddLocalShopStream: {}", e);
-                                if let Some(system) = System::try_current() {
-                                    system.stop()
-                                }
-                                return;
-                            }
-                        }
-                        local_shop_id += 1;
-                    }
-                }
+fn handle_communication_loop(
+    rx_from_input: mpsc::Receiver<String>,
+    listener: TcpListener,
+    order_handler: Addr<OrderHandler>,
+) -> Result<(), String> {
+    let mut local_shop_id = 0;
+    loop {
+        if is_exit_required(&rx_from_input) {
+            return Ok(());
+        }
+
+        if let Ok((stream, stream_addr)) = listener.accept() {
+            if let Ok(async_stream) = AsyncTcpStream::from_std(stream) {
+                info!(" [{:?}] Cliente conectado", stream_addr);
+                handle_connected_local_shop(async_stream, &order_handler, local_shop_id);
+                local_shop_id += 1;
             }
-        } else {
-            error!(" Error al intentar bindear el puerto");
+        }
+    }
+}
+
+fn handle_connected_local_shop(
+    async_stream: AsyncTcpStream,
+    order_handler: &Addr<OrderHandler>,
+    local_shop_id: u32,
+) {
+    let (read_half, write_half) = split(async_stream);
+
+    let sl_middleman_addr = SLMiddleman::create(|ctx| {
+        SLMiddleman::add_stream(LinesStream::new(BufReader::new(read_half).lines()), ctx);
+        SLMiddleman {
+            local_shop_write_stream: Arc::new(Mutex::new(write_half)),
+            local_shop_id,
+        }
+    });
+
+    match order_handler.try_send(AddSLMiddlemanAddr {
+        sl_middleman_addr: sl_middleman_addr.clone(),
+        local_shop_id,
+    }) {
+        Ok(_) => {}
+        Err(e) => {
+            error!(
+                "Error while sending AddLocalShopStream to OrderHandler: {}",
+                e
+            );
             if let Some(system) = System::try_current() {
                 system.stop()
             }
+            return;
         }
-    })
+    }
+}
+
+fn is_exit_required(rx_from_input: &mpsc::Receiver<String>) -> bool {
+    match rx_from_input.try_recv() {
+        Ok(msg) => {
+            if msg == EXIT_MSG {
+                info!("Received exit msg from input handler, stopping listener");
+                if let Some(system) = System::try_current() {
+                    system.stop()
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+        Err(_) => return false,
+    }
 }
